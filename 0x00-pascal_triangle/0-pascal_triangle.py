9b37b87d8e4df3c9c179f4977ddbc1a388f00251
RichardMiruka <richardonsare15@gmail.com>
2024-03-05 19:46:16 +0300

    Module to find Pascal's Triangle integers

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """
-Pascal Trangle
+Pascal Triangle
 """
 
 
 def pascal_triangle(n):
-    '''
-    Creates a list of lists of integers in a Pascal's triangle
+    """
+    Creates a list of lists of integers in Pascal's triangle
     of a given integer.
-    '''
+    """
     if n <= 0:
         return []
-    else:
-        res = []
-        for i in range(n):
-            if len(res) == 0:
-                res.append([1])
-            else:
-                row = [1]
-                for j in range(1, len(res[-1])):
-                    row.append(res[-1][j] + res[-1][j - 1])
-                row.append(1)
-                res.append(row)
-        return res
+
+    result = []
+    for i in range(n):
+        if not result:
+            result.append([1])
+        else:
+            row = [1]
+            for j in range(1, len(result[-1])):
+                row.append(result[-1][j] + result[-1][j - 1])
+            row.append(1)
+            result.append(row)
+
+    return result
