da3110bb89ba14d676468c0ecbe2ae5c38020ade
RichardMiruka <richardonsare15@gmail.com>
2024-03-20 13:08:50 +0300

    Function that calculates the minimum operations to copy and paste letters

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,12 +4,12 @@ Function that calculates the min operations to copy and paste letters
 """
 
 
-def minOperations(n): # 0-minoperations.py
-    nOpe = 0 # number of operations
-    minOpe = 2 # minimum operation needed for each pair
-    while n > 1: # while n is greater than 1, keep going until it's not
-        while n % minOpe == 0: # while n is divisible by minOpe
-            nOpe += minOpe # increment nOpe by minOpe 
-            n /= minOpe # divide n by minOpe
-        minOpe += 1 # increment minOpe by 1
-    return nOpe # return nOpe which will be equal to the number of pairs minus 1
+def minOperations(n):
+    nOpe = 0
+    minOpe = 2
+    while n > 1:
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe
